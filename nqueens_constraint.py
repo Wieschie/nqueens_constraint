0d95a56d633c439af95f1e1183cf94cfddebd833
Wieschie <supernova190@gmail.com>
2018-04-05 08:53:07 -0500

    Fix each queen to a unique row to save time and money.

--- nqueens_constraint.py
+++ nqueens_constraint.py
@@ -2,43 +2,39 @@ from constraint import *
 
 problem = Problem()
 
-board_size = 5
-nqueens = 5
+board_size = 10
 
-var_list = range(nqueens)
-var_list = [f"Q{v}" for v in var_list]
-# list of tuples for all coordinate locations
-coord_list = [(x,y) for x in range(board_size) for y in range(board_size) ]
+queen_list = range(board_size)
+# enforce different columns by restricting each queen to only move within 1 column.
+coord_list = [[(x,y) for y in range(board_size)] for x in range(board_size)]
 
-problem.addVariables(var_list, coord_list)
-problem.addConstraint(AllDifferentConstraint())
-for queen_1 in var_list:
-    for queen_2 in var_list:
+for q,c in zip(queen_list, coord_list):
+    problem.addVariable(q, c)
+
+for queen_1 in queen_list:
+    for queen_2 in queen_list:
         if queen_1 < queen_2:
-            # must be in different columns
-            problem.addConstraint(lambda q1, q2: q1[0] != q2[0], (queen_1, queen_2))
             # must be in different rows
             problem.addConstraint(lambda q1, q2: q1[1] != q2[1], (queen_1, queen_2))
             # cannot be diagonal
             problem.addConstraint(lambda q1, q2: abs(q1[0]-q2[0]) != abs(q1[1]-q2[1]), (queen_1, queen_2))
 
+
 solutions = problem.getSolutions()
-unique_solutions = set()
-for s in solutions:
-    unique_solutions.add(frozenset(s.values()))
 
 def print_horiz_line():
     print(" ---" * board_size)
 
-for i,s in enumerate(unique_solutions):
-    print(f"\n\nSolution {i}")
+for i,s in enumerate(solutions):
+    print(f"\n\nSolution {i+1}")
     for y in range(board_size):
         print_horiz_line()
         for x in range(board_size):
             cell = "|   "
-            for c in s:
+            for c in s.values():
                 if c == (x,y):
                     cell = f"| Q "
+                    break
             print(cell, end='')
         print("|   ")
     print_horiz_line()
